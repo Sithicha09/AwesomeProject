b54d4b54c5a29abc338467ca256bd20d4030a5d6
Sithicha09 <125942059+Sithicha09@users.noreply.github.com>
2023-11-30 14:57:26 +0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+import Welcome from './screens/Welcome';
+import Ex01 from './screens/Week2/Ex01';
+import Ex02 from './screens/Week2/Ex02';
+import Ex03 from './screens/Week2/Ex03';
+import Ex04 from './screens/Week2/Ex04';
+import Ex05 from './screens/Week2/Ex05';
+import Ex06 from './screens/Week2/Ex06';
+import Ex07 from './screens/Week2/Ex07';
+import Ex08 from './screens/Week2/Ex08';
+import Ex09 from './screens/Week2/Ex09';
+import Greeting from './screens/components/Greeting';
+
+
+export default function App() {
+  return (
+    //<Welcome />
+    //<Ex01 />
+    //<Ex02 />
+    //<Ex03 />
+    // <Ex04 />
+    //<Ex05 />
+    // <Ex06 />
+    // <Ex07 />
+    //<Ex08 />
+    <Ex09 />
+    // <View style={{alignItems: 'center', top: 50}}>
+    //     <Greeting name='Rexxar' />
+    //     <Greeting name='Jaina' />
+    //     <Greeting name='Valeera' />
+    //   </View>
+  );
+}
---
+++ screens/Week3/Travel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+
+export default function Travel() {
+    return (
+        <ScrollView>
+            <View style={{ flex: 1, paddingTop : 50  }}>
+                <Text> Week 3 </Text>
+            </View>
+        </ScrollView>
+    );
+}
