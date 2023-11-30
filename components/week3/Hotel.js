2419c8e7ac4b033341d5264756c6476c3bec4ce5
Sithicha09 <125942059+Sithicha09@users.noreply.github.com>
2023-11-30 16:25:20 +0700

    week3

--- components/week3/Hotel.js
+++ components/week3/Hotel.js
@@ -5,6 +5,27 @@ import { View } from 'react-native';
 export default function Hotel() {
     return (
         <View style={{ }}>
+            {/* View1 */}
+            <View style={{ }} >
+
+            </View>
+            {/* View2 */}
+            <View style={{ }} >
+
+            </View>
+            {/* View3 */}
+            <View style={{ }} >
+
+            </View>
+            {/* View4 */}
+            <View style={{ }} >
+
+            </View>
+            {/* View5 */}
+            <View style={{ }} >
+
+            </View>
+            
             
         </View>    
     );
